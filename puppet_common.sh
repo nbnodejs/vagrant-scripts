911ad1aecdb52a784c0aa883f9f5bc3cf88ef381
Neil <neil.bottomley@blueyonder.co.uk>
2020-10-08 13:09:04 +0100

    Conditional added to common to skip puppetlabs install if already installed

--- puppet_common.sh
+++ puppet_common.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-#Install puppet labs initial rpm to set things up
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+#Install puppet labs initial rpm to set things up if not installed already
 
+rpm -q puppetlabs-release > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+  rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+fi
