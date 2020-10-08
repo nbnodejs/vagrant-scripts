86111bc0d0f02f356e0328d6e64e298bcfa8c890
Neil <neil.bottomley@blueyonder.co.uk>
2020-10-08 14:47:19 +0100

    update to scripts

--- puppet_common.sh
+++ puppet_common.sh
@@ -8,20 +8,3 @@ if [ $? -ne 0 ]
 then
   rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 fi
-
-PENVS="production"
-
-for NEWENV in ${PENVS}
-do
-    MODULEDIR="/etc/puppet/environments/${NEWENV}/modules"
-    MANIFESTDIR="/etc/puppet/environments/${NEWENV}/manifests"
-    ENVCONF="/etc/puppet/environments/${NEWENV}/environment.conf"
-
-    [ ! -d ${MODULEDIR} ] && mkdir -p ${MODULEDIR}
-    [ ! -d ${MANIFESTDIR} ] && mkdir -p ${MANIFESTDIR}
-
-    echo "modulepath=${MODULEDIR}" > ${ENVCONF}
-    echo "environment_timeout=5s" >> ${ENVCONF}
-done
-
-mv /vagrant/puppets.conf /etc/puppet/puppet.conf
--- puppet_master.sh
+++ puppet_master.sh
@@ -2,3 +2,20 @@
 
 #Install puppet server package
 yum install -y puppetserver
+
+PENVS="production"
+
+for NEWENV in ${PENVS}
+do
+    MODULEDIR="/etc/puppet/environments/${NEWENV}/modules"
+    MANIFESTDIR="/etc/puppet/environments/${NEWENV}/manifests"
+    ENVCONF="/etc/puppet/environments/${NEWENV}/environment.conf"
+
+    [ ! -d ${MODULEDIR} ] && mkdir -p ${MODULEDIR}
+    [ ! -d ${MANIFESTDIR} ] && mkdir -p ${MANIFESTDIR}
+
+    echo "modulepath=${MODULEDIR}" > ${ENVCONF}
+    echo "environment_timeout=5s" >> ${ENVCONF}
+done
+
+mv /vagrant/puppets.conf /etc/puppet/puppet.conf
