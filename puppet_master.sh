cb3ba3384d920b3ea15c1d962ac65c3fd11277ae
Neil <neil.bottomley@blueyonder.co.uk>
2020-10-08 12:59:15 +0100

    Master script with removed puppet labs rpm install

--- puppet_master.sh
+++ puppet_master.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-#Install puppet labs initial rpm to set things up
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-
 #Install puppet server package
 yum install -y puppetserver
